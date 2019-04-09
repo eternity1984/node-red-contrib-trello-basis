c76f034ddb3e59242488b6516a4427bed96afdd2
yu_inoue <eternity.1984.10.04@gmail.com>
2019-04-09 16:43:21 +0900

    add pathParam field

--- node/trello.js
+++ node/trello.js
@@ -5,6 +5,15 @@ module.exports = function(RED) {
         constructor(definition) {
             super(RED, definition);
         }
+        getPathParams(RED, msg, definition) {
+            var paramType = definition.pathParamType;
+            if (paramType === "full") {
+                return msg;
+            } else {
+                var param = definition.pathParam;
+                return RED.util.evaluateNodeProperty(param, paramType, this, msg);
+            }
+        }
         getQueryParams(RED, msg, definition) {
             var query = msg.query || definition.query || {};
             if (typeof query !== "object") {
